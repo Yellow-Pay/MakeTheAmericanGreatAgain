1be7bb86e5df2bd0b78e6f562f2dcaeda4a85f88
杨森 <sen.yang96@outlook.com>
2019-08-26 09:10:20 -0700

    add concurrency read and wirte test

--- shm/RingBuffer.h
+++ shm/RingBuffer.h
@@ -9,7 +9,8 @@
 //  ----------------------------------------------------------------
 
 #define SHM_SIZE 32
-#define SHM_DATA_SIZE (SHM_SIZE - 3 * sizeof(uint32_t))
+#define METADATA_SIZE 3
+#define SHM_DATA_SIZE (SHM_SIZE - METADATA_SIZE * sizeof(uint32_t))
 #define GET_HEAD(rb) ((uint32_t *)(rb->address))[0]
 #define GET_TAIL(rb) ((uint32_t *)(rb->address))[1]
 #define GET_OLDTAIL(rb) ((uint32_t *)(rb->address))[2]
