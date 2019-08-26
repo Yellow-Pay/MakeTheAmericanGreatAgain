bd928414f5b2abbd720a93cc2a028a858b796d70
杨森 <sen.yang96@outlook.com>
2019-08-25 19:41:10 -0700

    add shared memory support

--- workspace/io.cpp
+++ workspace/io.cpp
@@ -1,19 +1,104 @@
-#include <string.h>
+#include <cstring>
 #include <iostream>
 #include <map>
 #include <cassert>
 #include <queue>
 #include <sys/shm.h>
+#include <cstdint>
 
 using namespace std;
 
 #define SHM_SIZE 4096
+#define get_idx(srcPort, destPort) srcPort * 65536 + destPort
+
+struct RingBuffer {
+	int shmid;
+	char* address;
+	char* content;
+	RingBuffer(key_t key) {
+		shmid = shmget(key, SHM_SIZE, 0666|IPC_CREAT);
+		address = (char*) shmat(shmid, (void*)0, 0);
+		char** data = (char**)address;
+		data[0] = (char*)&data[2];
+		data[1] = (char*)&data[2];
+		content = (char*)&data[2];
+	}
+	void destroy() {
+		// destroy the shared memory 
+    	shmctl(shmid,IPC_RMID,NULL); 
+	}
+	~RingBuffer() {
+		shmdt(address);
+	}
+	/* Head, Tail, content */
+	void* getHead() {
+		char** data = (char**) address;
+		return data[0];
+	}
+	void moveHead(uint32_t len) {
+		char** data = (char**) address;
+		data[0] += len;		
+	}
+	void* getTail() {
+		char** data = (char**) address;
+		return data[1];
+	}
+	void moveTail(uint32_t len) {
+		char** data = (char**) address;
+		data[1] += len;		
+	}
+
+	int read(int len, char *output) {
+		char* head = (char*)getHead();
+		memcpy(output, head, len);
+		moveHead(len);
+		// TODO: add circle
+		// TODO: add multi thread support
+		return len;
+	}
+
+	int write(int len, char *input) {
+		char* tail = (char*)getTail();
+		memcpy(tail, input, len);
+		moveTail(len);
+		// TODO: add circle
+		// TODO: add multi thread support
+		return len;
+	}
+};
 
 typedef struct Connection {
+	uint32_t src;
+	uint32_t dst;
+	RingBuffer readRB, writeRB;
+	Connection(uint32_t src_port, uint32_t dst_port) : src(src_port), dst(dst_port), readRB(get_idx(src, dst)), writeRB(get_idx(dst, src)) {
+	}
+	~Connection() {
+		// 读者负责关
+		readRB.destroy();
+	}
+	int read(int len, char *output) {
+		return readRB.read(len, output);
+	}
+	int write(int len, char *input) {
+		return writeRB.write(len, input);
+	}
+
 	bool idle;
 	void *address;
 	uint64_t length;
 } Connection_t;
+
+void testConnection(int argc, char* argv[]) {
+	assert(argc == 4);
+	uint32_t src = atoi(argv[1]), dst = atoi(argv[2]);
+	Connection c(src, dst);
+    cout << "Data written in memory: " << argv[3] << "\n"; 
+	c.write(5, argv[3]);
+	char buf[100];
+	c.read(5, buf);
+    cout << "Data read in memory: " << buf << "\n"; 
+}
 
 typedef struct Pool {
 	Connection_t *connections;
@@ -72,8 +157,6 @@ Connection_t *addConnection(Pool_t *pool, uint32_t key) {
 Pool_t *ConnectionPool;
 map<int, Connection_t *> ConnectionMap;
 
-#define get_idx(srcPort, destPort) srcPort * 65536 + destPort
-
 int openConnection(uint32_t srcPort, uint32_t destPort) {
 	uint32_t idx = get_idx(srcPort, destPort);
 	if (ConnectionMap.find(idx) != ConnectionMap.end()) {
@@ -93,18 +176,20 @@ int closeConnection(uint32_t srcPort, uint32_t destPort) {
 	}
 	Connection_t *conn = ConnectionMap[idx];
 	conn->idle = true;
+	shmdt(conn->address);
 	uint32_t idle_idx = (conn - ConnectionPool->connections);
 	ConnectionPool->idle_list.push(idle_idx);
 	ConnectionMap.erase(idx);
 	return 0;
 }
 
-int main() {
-	ConnectionPool = init(8);
-	ConnectionPool->idle_list.push(0);
-	assert(openConnection(1, 2) == 0);
-	assert(openConnection(2, 1)  == 0);
-	assert(openConnection(3, 2)  == 0);
-	closeConnection(2, 1);
+int main(int argc, char* argv[]) {
+	testConnection(argc, argv);
+	// ConnectionPool = init(8);
+	// ConnectionPool->idle_list.push(0);
+	// assert(openConnection(1, 2) == 0);
+	// assert(openConnection(2, 1)  == 0);
+	// assert(openConnection(3, 2)  == 0);
+	// closeConnection(2, 1);
 	return 0;
 }
